61aaf159ee8d5b14c508cbd49b3cfd92b88cc54e
Bill Donovan <williamcdonovan@gmail.com>
2017-08-12 19:20:52 -0600

    added express app

---
+++ server.js
@@ -0,0 +1,20 @@
+// ==============================================================================
+//Dependencies
+// ==============================================================================
+var express = require ("express");
+var bodyParser = require ("body-parser");
+var path = require ("path");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// ==============================================================================
+// Tells node that we are creating an "express" server
+var app = express();
+// Sets an initial port. 
+var PORT = process.env.PORT || 8080;
+// BodyParser makes it possible for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
