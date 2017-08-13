f3903e888438137dd55eae83e3ec0280dff451e2
Bill Donovan <williamcdonovan@gmail.com>
2017-08-13 16:40:30 -0600

    added friendsArray

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsArray = [
+	{
+		name: "Bluto",
+		photoURL: "https://yeslekjones.files.wordpress.com/2012/11/bluto3.jpg",
+		scores:[
+		    5,
+		    1,
+		    4,
+		    4,
+		    5,
+		    1,
+		    2,
+		    5,
+		    4,
+		    1
+		]
+	},
+];
+
+
+module.exports = friendsArray;
